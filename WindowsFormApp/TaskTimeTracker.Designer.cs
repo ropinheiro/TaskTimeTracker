936e989e5d7c943b4ce036ef01a81ddaa7a9d708
Rui Pinheiro <rui.pinheiro@together.pt>
2020-04-17 18:38:20 +0100

    #1 | First base commit with existing features

---
+++ WindowsFormApp/TaskTimeTracker.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace TaskTimeTracker
+{
+    partial class TaskTimeTracker
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTask = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.tmrTaskTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTask
+            // 
+            this.lblTask.AutoSize = true;
+            this.lblTask.Location = new System.Drawing.Point(12, 16);
+            this.lblTask.Name = "lblTask";
+            this.lblTask.Size = new System.Drawing.Size(49, 25);
+            this.lblTask.TabIndex = 0;
+            this.lblTask.Text = "Task:";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(460, 16);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(54, 25);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "Time:";
+            // 
+            // tmrTaskTimer
+            // 
+            this.tmrTaskTimer.Interval = 1000;
+            this.tmrTaskTimer.Tick += new System.EventHandler(this.TaskTimerEventProcessor);
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAddTask.Location = new System.Drawing.Point(10, 100);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(660, 50);
+            this.btnAddTask.TabIndex = 1;
+            this.btnAddTask.Text = "Add";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // TaskTimeTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 152);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblTask);
+            this.Name = "TaskTimeTracker";
+            this.Text = "Task Time Tracker @ Estamos Juntos 2020 - RMP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTask;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer tmrTaskTimer;
+        private System.Windows.Forms.Button btnAddTask;
+    }
+}
+
---
+++ WindowsFormApp/TaskTimeTracker.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TaskTimeTracker
+{
+    public partial class TaskTimeTracker : Form
+    {
+        public List<TaskTracker> Tasks = new List<TaskTracker>();
+
+        public TaskTracker CurrentTask;
+
+        public TaskTimeTracker()
+        {
+            InitializeComponent();
+
+            InitTasks();
+        }
+
+        private void btnStopStart_Click( object sender, EventArgs e )
+        {
+            Button clickedButton = ( (Button)sender );
+
+            if ( tmrTaskTimer.Enabled )
+            {
+                CurrentTask.TaskTime.BackColor = Color.Khaki;
+
+                if ( IsSameTask( clickedButton ) )
+                {
+                    tmrTaskTimer.Stop();
+                }
+                else
+                {
+                    SetCurrentTaskByClickedButton( clickedButton );
+                }
+            }
+            else
+            {
+                SetCurrentTaskByClickedButton( clickedButton );
+                tmrTaskTimer.Start();
+            }
+        }
+
+        private bool IsSameTask( Button clickedButton )
+        {
+            if ( CurrentTask == null )
+                return false;
+
+            return clickedButton == CurrentTask.ToggleButton;
+        }
+
+        private void SetCurrentTaskByClickedButton( Button clickedButton )
+        {
+            CurrentTask = GetTaskByClickedButton( clickedButton );
+            CurrentTask.TaskTime.BackColor = Color.LawnGreen;
+        }
+
+        private TaskTracker GetTaskByClickedButton( Button clickedButton )
+        {
+            foreach ( TaskTracker task in Tasks )
+            {
+                if ( clickedButton == task.ToggleButton
+                    || clickedButton == task.RemoveButton )
+                {
+                    return task;
+                }
+            }
+
+            throw new Exception( "This button is not associated to a Task!" );
+        }
+
+        private void InitTasks()
+        {
+            //List<string> taskDescriptions = new List<string>
+            //{
+            //    string.Empty, string.Empty, string.Empty, string.Empty
+            //};
+
+            //List<string> taskDescriptions = new List<string>
+            //{
+            //    "Talks with partners",
+            //    "#administrative stuff",
+            //    "SPV: tickets",
+            //    "SPV: customer / team management",
+            //    "Learning: JIRA courses",
+            //    "Personal: time with daughters",
+            //    string.Empty,
+            //    string.Empty,
+            //    string.Empty,
+            //    string.Empty,
+            //};
+
+            List<string> taskDescriptions = new List<string>
+            {
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+            };
+
+            foreach ( string description in taskDescriptions )
+                AddNewTask( description );
+        }
+
+        private void TaskTimerEventProcessor( object sender, EventArgs e )
+        {
+            CurrentTask.AddSeconds( 1 );
+        }
+
+        private void btnRemoveTask_Click( object sender, EventArgs e )
+        {
+            TaskTracker taskToRemove =
+                GetTaskByClickedButton( (Button)sender );
+
+            // TODO: move the limit elsewhere (e.g. contants? Configuration?)
+            DialogResult confirmResult = MessageBox.Show(
+                $"Are you sure to task '{taskToRemove.TaskDescription.Text}'",
+                "Confirmation",
+                MessageBoxButtons.YesNo );
+
+            if ( confirmResult == DialogResult.Yes )
+            {
+                RemoveTask( taskToRemove );
+            }
+        }
+
+        private void RemoveTask( TaskTracker taskToRemove )
+        {
+            // Adjust positions of all tasks coming after the
+            // task to be removed (move them up one position).
+            for ( int i = Tasks.Count - 1; i > taskToRemove.TaskNumber - 1; i-- )
+            {
+                // TODO: move the task controls to a panel so that them
+                // are easier to remove by removing the panel instead of
+                // each control individually.
+                Tasks[ i ].TaskNumber = Tasks[ i - 1 ].TaskNumber;
+                Tasks[ i ].TaskDescription.Location =
+                    Tasks[ i - 1 ].TaskDescription.Location;
+                Tasks[ i ].TaskTime.Location =
+                    Tasks[ i - 1 ].TaskTime.Location;
+                Tasks[ i ].ToggleButton.Location =
+                    Tasks[ i - 1 ].ToggleButton.Location;
+                Tasks[ i ].RemoveButton.Location =
+                    Tasks[ i - 1 ].RemoveButton.Location;
+            }
+
+            Controls.Remove( taskToRemove.TaskDescription );
+            Controls.Remove( taskToRemove.TaskTime );
+            Controls.Remove( taskToRemove.ToggleButton );
+            Controls.Remove( taskToRemove.RemoveButton );
+            Tasks.Remove( taskToRemove );
+
+            AdjustFooter();
+        }
+
+        private void btnAddTask_Click( object sender, EventArgs e )
+        {
+            // TODO: move the limit elsewhere (e.g. contants? Configuration?)
+            if ( Tasks != null && Tasks.Count >= 15 )
+            {
+                MessageBox.Show( "Too many tasks!", "Not allowed" );
+            }
+            else
+            {
+                AddNewTask( string.Empty );
+            }
+        }
+
+        private void AddNewTask( string description )
+        {
+            // Base task
+            TaskTracker newTask = new TaskTracker()
+            {
+                TaskNumber = GetNextTaskNumber()
+            };
+            int tabIndex = GetCurrentTabIndex();
+
+            // TODO: move this somewhere (e.g. to constants?)
+            int descriptionWidth = 450;
+            int timeWidth = 95;
+            int buttonWidth = 50;
+            int removeButtonWidth = 50;
+            int lineHeight = 30;
+            int verticalSpace = 10;
+            int horizontalSpace = 5;
+            int leftmostCoordinate = 10;
+            int topmostCoordinate = 50;
+
+            // Construct Task Description control
+            int topCoordinate = topmostCoordinate +
+                ( ( newTask.TaskNumber - 1 ) * ( lineHeight + verticalSpace ) );
+
+            newTask.TaskDescription = new TextBox
+            {
+                Location = new Point( leftmostCoordinate, topCoordinate ),
+                Size = new Size( descriptionWidth, lineHeight ),
+                Text = description,
+                TabIndex = ++tabIndex
+            };
+
+            // Construct Task Time control
+            int taskTimeLeftCoordinate =
+                leftmostCoordinate + descriptionWidth + horizontalSpace;
+
+            newTask.TaskTime = new TextBox
+            {
+                ReadOnly = true,
+                BackColor = Color.LightCoral,
+                ForeColor = Color.Black,
+                Location = new Point( taskTimeLeftCoordinate, topCoordinate ),
+                Size = new Size( timeWidth, lineHeight ),
+                TabIndex = ++tabIndex,
+                Text = "00:00:00",
+                TextAlign = HorizontalAlignment.Center
+            };
+
+            // Construct Task Button control
+            int buttonLeftCoordinate =
+                taskTimeLeftCoordinate + timeWidth + horizontalSpace;
+
+            newTask.ToggleButton = new Button
+            {
+                Location = new Point( buttonLeftCoordinate, topCoordinate ),
+                Size = new Size( buttonWidth, lineHeight ),
+                TabIndex = ++tabIndex,
+                Text = "GO",
+                UseVisualStyleBackColor = true
+            };
+            newTask.ToggleButton.Click +=
+                new EventHandler( btnStopStart_Click );
+
+            // Construct Remove Button control
+            int removeButtonLeftCoordinate =
+                buttonLeftCoordinate + buttonWidth + horizontalSpace;
+
+            newTask.RemoveButton = new Button
+            {
+                Location = new Point( removeButtonLeftCoordinate, topCoordinate ),
+                Size = new Size( removeButtonWidth, lineHeight ),
+                TabIndex = ++tabIndex,
+                Text = "DEL",
+                UseVisualStyleBackColor = true
+            };
+            newTask.RemoveButton.Click +=
+                new EventHandler( btnRemoveTask_Click );
+
+            // Add new controls to the Form and Task to the list
+            Tasks.Add( newTask );
+            Controls.Add( newTask.TaskDescription );
+            Controls.Add( newTask.TaskTime );
+            Controls.Add( newTask.ToggleButton );
+            Controls.Add( newTask.RemoveButton );
+
+            AdjustFooter();
+        }
+
+        private int GetNextTaskNumber()
+        {
+            if ( Tasks == null || Tasks.Count == 0 )
+                return 1;
+
+            return Tasks.Last().TaskNumber + 1;
+        }
+
+        private int GetCurrentTabIndex()
+        {
+            if ( Tasks == null || Tasks.Count == 0 )
+                return 0;
+
+            return Tasks.Last().RemoveButton.TabIndex;
+        }
+
+        private void AdjustFooter()
+        {
+            // TODO: move the magic numbers to constants
+
+            TaskTracker lastTask = Tasks.Last();
+
+            // Adjust Add Button to be after last Task
+            int titleHeight =
+                RectangleToScreen( ClientRectangle ).Top - Top;
+
+            btnAddTask.Top =
+                lastTask.TaskDescription.Top
+                + lastTask.TaskDescription.Height
+                + 10;
+
+            // Adjust Form's height to the contents
+            Height = titleHeight
+                + btnAddTask.Top
+                + btnAddTask.Height
+                + 20;
+        }
+    }
+
+    public class TaskTracker
+    {
+        public long TimeSpentInSeconds;
+        public int TaskNumber;
+
+        public TextBox TaskDescription;
+        public TextBox TaskTime;
+        public Button ToggleButton;
+        public Button RemoveButton;
+
+        public TaskTracker()
+        {
+            TimeSpentInSeconds = 0;
+            TaskNumber = 1;
+        }
+
+        public void AddSeconds( int seconds )
+        {
+            TimeSpentInSeconds += seconds;
+
+            TimeSpan time = TimeSpan
+                .FromSeconds( TimeSpentInSeconds );
+
+            TaskTime.Text = time.ToString( @"hh\:mm\:ss" );
+        }
+
+        public override string ToString()
+        {
+            return $"Task {TaskNumber} | {TaskDescription.Text}";
+        }
+    }
+}
